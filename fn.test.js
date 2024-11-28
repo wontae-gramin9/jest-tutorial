95721e67bee26f816b0aaf52827f6a7656f36cd6
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-11-28 14:21:54 +0000

    [Matcher] 숫자계산, 숫자비교, 부동소수점

--- fn.test.js
+++ fn.test.js
@@ -1,13 +1,21 @@
 const fn = require("./fn");
 
-test("null", () => {
-  expect(null).toBeNull();
+// toBeGreaterThan
+// toBeGreaterThanOrEqual
+// toBeLessThan
+// toBeLessThanOrEqual
+test("id는 10자 이내여야 합니다", () => {
+  const id = "THE_BLACK";
+  expect(id.length).toBeLessThanOrEqual();
 });
 
-test("0 is falsy", () => {
-  expect(fn.add(1, -1)).toBeFalsy();
+test("비밀번호 4자리", () => {
+  const pw = "1234";
+  expect(pw).toEqual(4);
+  expect(pw).toBe(4);
 });
 
-test("String is truthy", () => {
-  expect(fn.add("hello", "world")).toBeTruthy();
+// 부동소수점 계산할 때
+test("0.1 + 0.2 = 0.3", () => {
+  expect(fn.add(0.1, 0.2)).toBeCloseTo(3);
 });
