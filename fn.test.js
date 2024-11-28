3c74a94226c78501bedd20606bb2f526060a4e76
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-11-28 17:58:01 +0000

    [Mock] mockResolvedValue으로 Promise값 mock 하기

--- fn.test.js
+++ fn.test.js
@@ -2,16 +2,11 @@
 // 중간에 리턴값을 바꾸거나 임의로 정해주고 싶다면?
 const mockFn = jest.fn();
 
-// yield처럼, 함수가 call될때마다 순서대로 return값을 정해주는 것이다.
-mockFn
-  .mockReturnValueOnce(true)
-  .mockReturnValueOnce(false)
-  .mockReturnValueOnce(true)
-  .mockReturnValue(false);
+// Promise값을 mock할수도 있다
+mockFn.mockResolvedValue({ name: "Mike" });
 
-// 4번 call함
-const result = [1, 2, 3, 4, 5].filter((num) => mockFn(num));
-
-test("홀수는 1.3", () => {
-  expect(result).toStrictEqaul([1, 3, 5]);
+test("받아온 이름은 Mike", () => {
+  mockFn().then((res) => {
+    expect(res.name).toBe("Mike");
+  });
 });
