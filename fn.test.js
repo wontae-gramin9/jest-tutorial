828e6e74de6086a562508e4cae7d329e76766a2c
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-11-28 15:37:15 +0000

    [Async/Await] Callback, Promise보다 간단하게

--- fn.test.js
+++ fn.test.js
@@ -1,13 +1,6 @@
 const fn = require("./fn");
 
-test("Promise로 받아온 3초 후 이름은 Mike", () => {
-  return fn.getAge().then((age) => {
-    expect(age).toBe(30);
-  });
-  // Promise를 return해줘야하지만 기다린다.
-
-  // 혹은 return에 resolves matcher를 사용할수도 있다
-  return expect(fn.getAge()).resolves.toBe(30);
-  // 에러일 때 (에러메시지 정규표현식 매치)
-  return expect(fn.getAge()).rejects.toMatch(/error/i);
+test("async await는 더 간단한 3초 후 이름은 Mike", async () => {
+  const age = await fn.getAge();
+  expect(age).toBe(30);
 });
