e03029a65934ed8be3cb5ecd8d3a20373c82d5f4
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-11-28 16:00:55 +0000

    [각 테스트 전후 작업] beforeEach, afterEach

--- fn.test.js
+++ fn.test.js
@@ -1,6 +1,25 @@
 const fn = require("./fn");
 
-test("async await는 더 간단한 3초 후 이름은 Mike", async () => {
-  const age = await fn.getAge();
-  expect(age).toBe(30);
+let num = 0;
+
+// beforeEach: 모든 테스트 이전에 한번씩 실행
+// afterEach: 모든 테스트 이후에 한번씩 실행
+// 없다면, global변수인 num에 값이 계속 더해지기 때문에 실패한다
+beforeEach(() => {
+  num = 0;
+});
+
+test("0+1 = 1", () => {
+  num = fn.add(num, 1);
+  expect(num).toBe(1);
+});
+
+test("0+2 = 2", () => {
+  num = fn.add(num, 2);
+  expect(num).toBe(2);
+});
+
+test("0+3 = 3", () => {
+  num = fn.add(num, 3);
+  expect(num).toBe(3);
 });
