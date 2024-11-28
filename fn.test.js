15e5334f11c2b0093c7dad30ff847a90f20b57e6
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-11-28 16:07:10 +0000

    [각 테스트 전후 작업] db connection이나 network request로 한번 받아온 값에 대한 테스트를 할 때, before/afterEach로 하게되면 매 테스트마다 실행되어 시간을 잡아먹는다

--- fn.test.js
+++ fn.test.js
@@ -1,25 +1,22 @@
 const fn = require("./fn");
 
-let num = 0;
-
-// beforeEach: 모든 테스트 이전에 한번씩 실행
-// afterEach: 모든 테스트 이후에 한번씩 실행
-// 없다면, global변수인 num에 값이 계속 더해지기 때문에 실패한다
-beforeEach(() => {
-  num = 0;
+let user;
+beforeEach(async () => {
+  user = await fn.connectDb();
 });
 
-test("0+1 = 1", () => {
-  num = fn.add(num, 1);
-  expect(num).toBe(1);
+afterEach(async () => {
+  await fn.disconnectDb();
 });
 
-test("0+2 = 2", () => {
-  num = fn.add(num, 2);
-  expect(num).toBe(2);
+test("이름 Mike", () => {
+  expect(user.name).toBe("Mike");
 });
 
-test("0+3 = 3", () => {
-  num = fn.add(num, 3);
-  expect(num).toBe(3);
+test("나이 30", () => {
+  expect(user.age).toBe(30);
+});
+
+test("성별 남성", () => {
+  expect(user.gender).toBe("male");
 });
