fefd77b69475d7b14f0f14a56eafb95df2d35752
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-11-28 14:23:12 +0000

    [Matcher] 정규표현식 toMatch

--- fn.test.js
+++ fn.test.js
@@ -1,21 +1,6 @@
 const fn = require("./fn");
 
-// toBeGreaterThan
-// toBeGreaterThanOrEqual
-// toBeLessThan
-// toBeLessThanOrEqual
-test("id는 10자 이내여야 합니다", () => {
-  const id = "THE_BLACK";
-  expect(id.length).toBeLessThanOrEqual();
-});
-
-test("비밀번호 4자리", () => {
-  const pw = "1234";
-  expect(pw).toEqual(4);
-  expect(pw).toBe(4);
-});
-
-// 부동소수점 계산할 때
-test("0.1 + 0.2 = 0.3", () => {
-  expect(fn.add(0.1, 0.2)).toBeCloseTo(3);
+// 정규표현식 toMatch
+test("Hello world에 a가 있는가?", () => {
+  expect("Hello world").toMatch(/h/i);
 });
